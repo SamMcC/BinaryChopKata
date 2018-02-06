e26deaaf8c961e3eb47421d6bde95f5fd1d36198
Sam McCormack <sj.mccormack9@gmail.com>
2018-02-06 19:27:53 +0000

    Fixed not working code, felt dirty casting to int

--- src/main/java/BinarySearcher.java
+++ src/main/java/BinarySearcher.java
@@ -11,16 +11,16 @@ public class BinarySearcher {
     }
 
     public int searchForValueInArrayBySplit(int[] inputArray, int value){
-        int arrayLength = inputArray.length;
-        int currentAttempt = 1;
-        int currentIndex = arrayLength-1;
+        float arrayLength = inputArray.length;
+        float currentAttempt = 1;
+        int currentIndex = (int) arrayLength-1;
         int result = -1;
         while (numberOfAttemptsIsLessThanLengthOfArray(currentAttempt, arrayLength, currentIndex)) {
-            if (valueIsGreaterThanCurrentValue(inputArray[currentIndex], value)) {
-                currentIndex -= arrayLength / (currentAttempt * 2);
+            if (valueIsLessThanCurrentValue(inputArray[currentIndex], value)) {
+                currentIndex -= Math.ceil(arrayLength / (currentAttempt * 2));
                 currentIndex = correctIfTooHighOrTooLow(currentIndex, arrayLength);
-            } else if (valueIsLessThanCurrentValue(inputArray[currentIndex], value)) {
-                currentIndex += arrayLength / (currentAttempt * 2);
+            } else if (valueIsGreaterThanCurrentValue(inputArray[currentIndex], value)) {
+                currentIndex += Math.ceil(arrayLength / (currentAttempt * 2));
                 currentIndex = correctIfTooHighOrTooLow(currentIndex, arrayLength);
             }else{
                 return currentIndex;
@@ -31,18 +31,18 @@ public class BinarySearcher {
         return result;
     }
 
-    private int correctIfTooHighOrTooLow(int currentIndex, int arrayLength) {
+    private int correctIfTooHighOrTooLow(int currentIndex, float arrayLength) {
         if (currentIndex < 0){
             return 0;
         }else if(currentIndex > arrayLength-1){
-            return arrayLength-1;
+            return (int) arrayLength-1;
         }else{
             return currentIndex;
         }
     }
 
-    private boolean numberOfAttemptsIsLessThanLengthOfArray(int currentAttempt, int arrayLength, int currentIndex) {
-        return currentAttempt < arrayLength && arrayLength > 0 && currentIndex < arrayLength-1;
+    private boolean numberOfAttemptsIsLessThanLengthOfArray(float currentAttempt, float arrayLength, int currentIndex) {
+        return currentAttempt <= arrayLength && arrayLength > 0 && currentIndex < arrayLength;
     }
 
     private boolean valueIsGreaterThanCurrentValue(int currentValue, int requiredValue) {
